0cbbd680fc4091d3e576fe7fe77c52f684c331cd
kosty2979 <kosty2979@gmail.com>
2017-06-20 16:03:45 +0300

    added: post request test

--- 02/get-post-server/test/server.js
+++ 02/get-post-server/test/server.js
@@ -13,16 +13,46 @@ describe('server tests', () => {
         app.close(done);
     });
 
-    it('get index.html', (done) => {
+    describe('GET request', () =>
+        it('get index.html', (done) => {
+            request('http://localhost:8888/', (error, response, body) => {
+                if (error) return done(error);
 
-        request('http://localhost:8888', (error, response, body) => {
-            if (error) return done(error);
+                const fileContent = fs.readFileSync('public/index.html');
 
-            const fileContent = fs.readFileSync('public/index.html');
+                assert.equal(body, fileContent);
+                done();
+            });
+        })
+    );
 
-            assert.equal(body, fileContent);
-            done();
-        });
+    describe('POST request', () => {
+        it('POST send file fortest.txt', (done) => {
+            const file = fs.readFileSync('test/fortest.txt').toString();
+            const error = {
+                code: 404,
+                text: 'File not found'
+            };
 
-    });
+            request.post('http://localhost:8888/', {body: file},
+                function (err, res) {
+                    assert.equal(res.statusCode, error.code);
+                    assert.equal(res.body, error.text)
+
+
+                    done()
+                    // fs.readFile('files/fortest.txt', (err, content)=>{
+                    //     if (err){
+                    //          if (err) return done(err);
+                    //     }
+                    //
+                    //
+                    // })
+                })
+
+
+        })
+
+
+    })
 });
