6cd9bdeb53be7a7273698ce6b3f9293cba0ae70b
kosty2979 <kosty2979@gmail.com>
2017-06-21 12:48:59 +0300

    added: new test

--- 02/get-post-server/test/server.js
+++ 02/get-post-server/test/server.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fs = require('fs');
 const assert = require('assert');
 
-describe('server tests', () => {
+describe('Server tests:', () => {
     let app;
     before((done) => {
         app = server.listen(8888, done);
@@ -27,30 +27,50 @@ describe('server tests', () => {
     );
 
     describe('POST request', () => {
-        it('POST send file fortest.txt', (done) => {
-            const file = fs.readFileSync('test/fortest.txt').toString();
+
+        fs.unlink('files/small.txt', (err) => {
+        });
+        const file = fs.readFileSync('test/small.txt').toString();
+        const bigFile = fs.readFileSync('test/big.png').toString();
+
+        it('POST write file ', (done) => {
+            request.post('http://localhost:8888/small.txt', {body: file}, (err, res) => {
+                assert.equal(res.statusCode, 201);
+                fs.readFile('files/small.txt', (err, content) => {
+                    if (err) {
+                        if (err) return done(err);
+                    }
+                    assert.equal(file, content.toString())
+
+                });
+                done()
+            })
+        });
+
+        it('POST write exist file', (done) => {
             const error = {
-                code: 404,
-                text: 'File not found'
+                code: 409,
+                text: 'File exist'
             };
+            request.post('http://localhost:8888/small.txt', {body: file}, (err, res) => {
+                assert.equal(res.statusCode, error.code);
+                assert.equal(res.body, error.text)
 
-            request.post('http://localhost:8888/', {body: file},
-                function (err, res) {
-                    assert.equal(res.statusCode, error.code);
-                    assert.equal(res.body, error.text)
+                done()
+            })
+        });
 
+        it("POST write big file", (done) => {
+            const error = {
+                code: 413,
+                text: 'File is too big!'
+            };
+            request.post('http://localhost:8888/big.png', {body: bigFile}, (req, res) => {
+                assert.equal(res.statusCode, error.code);
+                assert.equal(res.body, error.text);
 
-                    done()
-                    // fs.readFile('files/fortest.txt', (err, content)=>{
-                    //     if (err){
-                    //          if (err) return done(err);
-                    //     }
-                    //
-                    //
-                    // })
-                })
-
-
+                done()
+            })
         })
 
 
